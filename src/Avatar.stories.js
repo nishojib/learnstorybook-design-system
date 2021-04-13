0fe7353e1be51fedeb8b0b4fc27805ca2788b9e6
Nur Islam <nishojib@gmail.com>
2021-04-13 18:15:54 -0400

    chromatic

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -1,17 +1,16 @@
-import React from "react";
-
-import { Avatar } from "./Avatar";
+import React from 'react';
+import { Avatar } from './Avatar';
 
 export default {
-  title: "Design System/Avatar",
+  title: 'Design System/Avatar',
   component: Avatar,
 };
 
 export const Standard = (args) => <Avatar {...args} />;
 Standard.args = {
-  size: "large",
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
+  size: 'large',
+  username: 'Tom Coleman',
+  src: 'https://avatars2.githubusercontent.com/u/132554',
 };
 
 export const Sizes = (args) => (
@@ -23,8 +22,8 @@ export const Sizes = (args) => (
   </div>
 );
 Sizes.args = {
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
+  username: 'Tom Coleman',
+  src: 'https://avatars2.githubusercontent.com/u/132554',
 };
 
 export const Initials = (args) => (
@@ -37,16 +36,16 @@ export const Initials = (args) => (
 );
 
 export const Loading = (args) => (
-    <div>
-      <Avatar {...args} size="large" />
-      <Avatar {...args} size="medium" />
-      <Avatar {...args} size="small" />
-      <Avatar {...args} size="tiny" />
-    </div>
-  );
-  Loading.args = {
-    loading: true,
-  };
+  <div>
+    <Avatar {...args} size="large" />
+    <Avatar {...args} size="medium" />
+    <Avatar {...args} size="small" />
+    <Avatar {...args} size="tiny" />
+  </div>
+);
+Loading.args = {
+  loading: true,
+};
 
 export const Large = (args) => (
   <div>
@@ -59,3 +58,13 @@ export const Large = (args) => (
     />
   </div>
 );
+
+const Template = (args) => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+Controls.args = {
+  loading: false,
+  size: 'tiny',
+  username: 'Dominic Nguyen',
+  src: 'https://avatars2.githubusercontent.com/u/263385',
+};
