6c801f49e9006102a21a49eb74952e1079e09687
Nur Islam <nishojib@gmail.com>
2021-04-14 07:06:34 -0400

    updated .gitignore [skip ci]

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import * as styles from './shared/styles';
-import * as global from './shared/global';
 import * as animation from './shared/animation';
+import * as global from './shared/global';
 import * as icons from './shared/icons';
-
-export { styles, global, animation, icons };
+import * as styles from './shared/styles';
 
 export * from './Avatar';
 export * from './Badge';
 export * from './Button';
 export * from './Icon';
 export * from './Link';
+export { styles, global, animation, icons };
